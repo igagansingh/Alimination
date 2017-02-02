9ffd61f35d96794205d0ac70689378fc744b17fb
Gagandeep Singh <igagandeep.s@gmail.com>
2017-02-02 14:26:15 +0530

    EnterDetails activity updated

--- app/src/main/java/com/gagandeep/developer/alimination/EnterDetails.java
+++ app/src/main/java/com/gagandeep/developer/alimination/EnterDetails.java
@@ -2,18 +2,38 @@ package com.gagandeep.developer.alimination;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class EnterDetails extends AppCompatActivity {
 
+    //cn - contact person
+    EditText name, cpOneName, cpOnePhone, cpTwoName, cpTwoPhone;
+    Button getData;
+    String nameString="", cpOneNameString="", cpOnePhoneString="", cpTwoNameString="", cpTwoPhoneString="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_details);
 
-        String name=getIntent().getExtras().getString("name");
-        String editText11=getIntent().getExtras().getString("editText11");
-        String phoneNumber=getIntent().getExtras().getString("phoneNumber");
-        String editText13=getIntent().getExtras().getString("editText13");
-        String editText12=getIntent().getExtras().getString("editText12");
+        name = (EditText) findViewById(R.id.name);
+        cpOneName = (EditText) findViewById(R.id.cponename);
+        cpOnePhone = (EditText) findViewById(R.id.cponephone);
+        cpTwoName = (EditText) findViewById(R.id.cptwoname);
+        cpTwoPhone = (EditText) findViewById(R.id.cptwophone);
+
+        getData = (Button) findViewById(R.id.button);
+
+        getData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nameString = name.getText().toString();
+                cpOneNameString = cpOneName.getText().toString();
+                cpOnePhoneString = cpOnePhone.getText().toString();
+                cpTwoNameString = cpTwoName.getText().toString();
+                cpTwoPhoneString = cpTwoPhone.getText().toString();
+            }
+        });
     }
 }
